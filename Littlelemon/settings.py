5ac09c6b82693230dde7fe6e0ea5165d41238f12
Ab <46964181+abd-koumare@users.noreply.github.com>
2024-08-07 19:47:59 +0000

    added djoser

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
 
     'restaurant.apps.RestaurantConfig',
     'api.apps.ApiConfig',
@@ -140,5 +141,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
 }
--- Littlelemon/urls.py
+++ Littlelemon/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('api/', include('api.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
